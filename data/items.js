f5aca63bdce4df68304dcb52fadad3bbcf1cbc51
Luis Eduardo Siman <siman@alunos.utfpr.edu.br>
2020-04-03 13:42:07 -0300

    Change data/items.json to data/items.js
    This project should stay simple, so I am not willing to start
    a HTTP server to store the mock json database. Reading the file
    directly from the client is proving to be too much of a headache
    (can't use fetch API, can't use XML requests, no way to read the
    filesystem directly).
    
    Therefore, I decided to hardcode the JSON data in a constant, that
    will be stored in a file under the data directory.

---
+++ data/items.js
@@ -0,0 +1,13 @@
+const ITEMS = {
+  rebirthItems: [
+    {
+      itemId: 1,
+      name: "The Sad Onion",
+      description: "Tears up",
+      effects: ["+0.7 Tears Up"],
+      type: "passive",
+      itemPool: ["item room"],
+      spriteName: "item1",
+    },
+  ],
+};
